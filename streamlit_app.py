aaab0bf4472e9491f9c5967d8680c7a798c785e1
jer019 <152777929+jer019@users.noreply.github.com>
2024-08-01 00:23:54 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,18 @@ ingredients_list=st.multiselect(
 )
 
 if ingredients_list:
-  
-  ingredients_string=''
-  
-  for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon", fruit_chosen)
-        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
-
-
-    my_insert_stmt = """ insert into smoothies.public.orders(INGREDIENTS,NAME_ON_ORDER)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-
+      
+      ingredients_string=''
+      
+      for fruit_chosen in ingredients_list:
+            ingredients_string += fruit_chosen + ' '
+            st.subheader(fruit_chosen + ' Nutrition Information')
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon", fruit_chosen)
+            fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+          
+      my_insert_stmt = """ insert into smoothies.public.orders(INGREDIENTS,NAME_ON_ORDER)
+                values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    
 
 time_to_insert=st.button('Submit Order')
     
